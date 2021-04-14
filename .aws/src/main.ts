9658d9994a1d2a959868a06cfcd678d2dc6e6022
Kelvin Yeboah <kyeboah@getpocket.com>
2021-04-14 15:09:56 -0400

    feat(migration): auto run migrations

--- .aws/src/main.ts
+++ .aws/src/main.ts
@@ -1,276 +1,34 @@
 import { Construct } from 'constructs';
-import {
-  App,
-  DataTerraformRemoteState,
-  RemoteBackend,
-  TerraformStack,
-} from 'cdktf';
-import {
-  AwsProvider,
-  DataAwsCallerIdentity,
-  DataAwsKmsAlias,
-  DataAwsRegion,
-  DataAwsSnsTopic,
-} from '../.gen/providers/aws';
+import { App, RemoteBackend, TerraformStack } from 'cdktf';
+import { AwsProvider } from '../.gen/providers/aws';
 import { config } from './config';
-import {
-  ApplicationRDSCluster,
-  ApplicationRedis,
-  PocketALBApplication,
-  PocketPagerDuty,
-  PocketVPC,
-} from '@pocket/terraform-modules';
+import { PocketVPC } from '@pocket/terraform-modules';
 import { PagerdutyProvider } from '../.gen/providers/pagerduty';
+import { createElasticache } from './elasticache';
+import { createRds } from './rds';
+import { createPocketAlbApplication } from './pocketAlbApplication';
 
 class CollectionAPI extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    new AwsProvider(this, 'aws', {
-      region: 'us-east-1',
-    });
+    new AwsProvider(this, 'aws', { region: 'us-east-1' });
 
-    new PagerdutyProvider(this, 'pagerduty_provider', {
-      token: undefined,
-    });
+    new PagerdutyProvider(this, 'pagerduty_provider', { token: undefined });
 
     new RemoteBackend(this, {
       hostname: 'app.terraform.io',
       organization: 'Pocket',
-      workspaces: [
-        {
-          prefix: `${config.name}-`,
-        },
-      ],
-    });
-
-    const incidentManagement = new DataTerraformRemoteState(
-      this,
-      'incident_management',
-      {
-        organization: 'Pocket',
-        workspaces: {
-          name: 'incident-management',
-        },
-      }
-    );
-
-    const pagerDuty = new PocketPagerDuty(this, 'pagerduty', {
-      prefix: config.prefix,
-      service: {
-        criticalEscalationPolicyId: incidentManagement.get(
-          'policy_backend_critical_id'
-        ),
-        nonCriticalEscalationPolicyId: incidentManagement.get(
-          'policy_backend_non_critical_id'
-        ),
-      },
-    });
-
-    const region = new DataAwsRegion(this, 'region');
-    const caller = new DataAwsCallerIdentity(this, 'caller');
-    const secretsManager = new DataAwsKmsAlias(this, 'kms_alias', {
-      name: 'alias/aws/secretsmanager',
-    });
-
-    const snsTopic = new DataAwsSnsTopic(this, 'backend_notifications', {
-      name: `Backend-${config.environment}-ChatBot`,
+      workspaces: [{ prefix: `${config.name}-` }],
     });
 
     const pocketVpc = new PocketVPC(this, 'pocket-vpc');
 
-    const { primaryEndpoint, readerEndpoint } = CollectionAPI.createElasticache(
-      this,
-      pocketVpc
-    );
+    const elasticache = createElasticache(this, pocketVpc);
 
-    const rds = CollectionAPI.createRDS(this, pocketVpc);
+    const rds = createRds(this, pocketVpc);
 
-    new PocketALBApplication(this, 'application', {
-      internal: true,
-      prefix: config.prefix,
-      alb6CharacterPrefix: config.shortName,
-      tags: config.tags,
-      cdn: false,
-      domain: config.domain,
-      containerConfigs: [
-        {
-          name: 'app',
-          portMappings: [
-            {
-              hostPort: 4004,
-              containerPort: 4004,
-            },
-          ],
-          envVars: [
-            {
-              name: 'ENVIRONMENT',
-              value: process.env.NODE_ENV, // this gives us a nice lowercase production and development
-            },
-            {
-              name: 'REDIS_PRIMARY_ENDPOINT',
-              value: primaryEndpoint,
-            },
-            {
-              name: 'REDIS_READER_ENDPOINT',
-              value: readerEndpoint,
-            },
-          ],
-          secretEnvVars: [
-            {
-              name: 'SENTRY_DSN',
-              valueFrom: `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/SENTRY_DSN`,
-            },
-            {
-              name: 'DB_HOST',
-              valueFrom: `${rds.secretARN}:host::`,
-            },
-            {
-              name: 'DB_USERNAME',
-              valueFrom: `${rds.secretARN}:username::`,
-            },
-            {
-              name: 'DB_PASSWORD',
-              valueFrom: `${rds.secretARN}:password::`,
-            },
-          ],
-        },
-        {
-          name: 'xray-daemon',
-          containerImage: 'amazon/aws-xray-daemon',
-          portMappings: [
-            {
-              hostPort: 2000,
-              containerPort: 2000,
-              protocol: 'udp',
-            },
-          ],
-          command: ['--region', 'us-east-1', '--local-mode'],
-        },
-      ],
-      codeDeploy: {
-        useCodeDeploy: true,
-        snsNotificationTopicArn: snsTopic.arn,
-      },
-      exposedContainer: {
-        name: 'app',
-        port: 4004,
-        healthCheckPath: '/.well-known/apollo/server-health',
-      },
-      ecsIamConfig: {
-        prefix: config.prefix,
-        taskExecutionRolePolicyStatements: [
-          //This policy could probably go in the shared module in the future.
-          {
-            actions: ['secretsmanager:GetSecretValue', 'kms:Decrypt'],
-            resources: [
-              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared`,
-              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared/*`,
-              secretsManager.targetKeyArn,
-            ],
-            effect: 'Allow',
-          },
-          //This policy could probably go in the shared module in the future.
-          {
-            actions: ['ssm:GetParameter*'],
-            resources: [
-              `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}`,
-              `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/*`,
-            ],
-            effect: 'Allow',
-          },
-        ],
-        taskRolePolicyStatements: [
-          {
-            actions: [
-              'xray:PutTraceSegments',
-              'xray:PutTelemetryRecords',
-              'xray:GetSamplingRules',
-              'xray:GetSamplingTargets',
-              'xray:GetSamplingStatisticSummaries',
-            ],
-            resources: ['*'],
-            effect: 'Allow',
-          },
-        ],
-        taskExecutionDefaultAttachmentArn:
-          'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy',
-      },
-
-      autoscalingConfig: {
-        targetMinCapacity: 2,
-        targetMaxCapacity: 10,
-      },
-      alarms: {
-        //TODO: When we start using this more we will change from non-critical to critical
-        http5xxError: {
-          threshold: 10,
-          evaluationPeriods: 2,
-          period: 600,
-          actions: [pagerDuty.snsNonCriticalAlarmTopic.arn],
-        },
-        httpLatency: {
-          evaluationPeriods: 2,
-          threshold: 500,
-          actions: [pagerDuty.snsNonCriticalAlarmTopic.arn],
-        },
-        httpRequestCount: {
-          threshold: 5000,
-          evaluationPeriods: 2,
-          actions: [pagerDuty.snsNonCriticalAlarmTopic.arn],
-        },
-      },
-    });
-  }
-
-  /**
-   * Creates the elasticache and returns the node address list
-   * @param scope
-   * @param pocketVpc
-   * @private
-   */
-  private static createElasticache(
-    scope: Construct,
-    pocketVpc: PocketVPC
-  ): { primaryEndpoint: string; readerEndpoint: string } {
-    const elasticache = new ApplicationRedis(scope, 'redis', {
-      //Usually we would set the security group ids of the service that needs to hit this.
-      //However we don't have the necessary security group because it gets created in PocketALBApplication
-      //So instead we set it to null and allow anything within the vpc to access it.
-      //This is not ideal..
-      //Ideally we need to be able to add security groups to the ALB application.
-      allowedIngressSecurityGroupIds: undefined,
-      node: {
-        count: config.cacheNodes,
-        size: config.cacheSize,
-      },
-      subnetIds: pocketVpc.privateSubnetIds,
-      tags: config.tags,
-      vpcId: pocketVpc.vpc.id,
-      prefix: config.prefix,
-    });
-
-    return {
-      primaryEndpoint:
-        elasticache.elasticacheReplicationGroup.primaryEndpointAddress,
-      readerEndpoint:
-        elasticache.elasticacheReplicationGroup.readerEndpointAddress,
-    };
-  }
-
-  private static createRDS(scope: Construct, pocketVpc: PocketVPC) {
-    return new ApplicationRDSCluster(scope, 'rds', {
-      prefix: config.prefix,
-      vpcId: pocketVpc.vpc.id,
-      subnetIds: pocketVpc.privateSubnetIds,
-      rdsConfig: {
-        databaseName: 'collections',
-        masterUsername: 'pkt_collections',
-        engine: 'aurora-mysql',
-        engineMode: 'serverless',
-      },
-      tags: config.tags,
-    });
+    createPocketAlbApplication(this, { elasticache, rds });
   }
 }
 
---
+++ .aws/src/rds.ts
@@ -0,0 +1,18 @@
+import { Construct } from 'constructs';
+import { ApplicationRDSCluster, PocketVPC } from '@pocket/terraform-modules';
+import { config } from './config';
+
+export function createRds(scope: Construct, pocketVpc: PocketVPC) {
+  return new ApplicationRDSCluster(scope, 'rds', {
+    prefix: config.prefix,
+    vpcId: pocketVpc.vpc.id,
+    subnetIds: pocketVpc.privateSubnetIds,
+    rdsConfig: {
+      databaseName: 'collections',
+      masterUsername: 'pkt_collections',
+      engine: 'aurora-mysql',
+      engineMode: 'serverless',
+    },
+    tags: config.tags,
+  });
+}
