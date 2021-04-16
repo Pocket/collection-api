2ab85cc974685c3f46ef94aa0c1668cf77fa390a
Daniel Brooks <bass_rock@me.com>
2021-04-16 13:19:39 -0700

    fixing types

--- src/dataLoaders/collectionLoader.ts
+++ src/dataLoaders/collectionLoader.ts
@@ -1,13 +1,15 @@
 import DataLoader from 'dataloader';
 import { getCollectionsBySlugs } from '../database/queries';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Collection } from '@prisma/client';
 import { client } from '../database/client';
 
 /**
  * Grabs all collections from the database
  * @param slugs
  */
-export const batchFetchBySlugs = async (slugs: string[]): Promise<any> => {
+export const batchFetchBySlugs = async (
+  slugs: string[]
+): Promise<Collection[]> => {
   const db: PrismaClient = client();
 
   return await getCollectionsBySlugs(db, slugs);
