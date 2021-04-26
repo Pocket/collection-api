01838b01fb19276de9d02045430f638a5636ec20
Kelvin Yeboah <kyeboah@getpocket.com>
2021-04-26 17:25:53 -0400

    [BACK-730] Fix upload mutation (#42)

--- .circleci/scripts/setup_aws.sh
+++ .circleci/scripts/setup_aws.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+sudo apt-get update && sudo apt-get install -y python3-pip
+pip3 install awscli-local awscli
+
 for Script in .docker/localstack/*.sh ; do
     bash "$Script"
 done
