2727a1654e8859ddbf28d9ec8d057e4b95a16624
Kelvin Yeboah <kyeboah@getpocket.com>
2021-04-27 17:40:58 -0400

    fix(s3 endpoint): only set s3 endpoint using env vars (#54)

--- src/aws/s3.ts
+++ src/aws/s3.ts
@@ -2,10 +2,7 @@ import { S3 } from 'aws-sdk';
 import config from '../config';
 
 export default new S3({
-  endpoint:
-    config.app.environment !== 'production'
-      ? config.aws.s3.endpoint
-      : undefined,
+  endpoint: config.aws.s3.localEndpoint,
   region: 'us-east-1',
   s3ForcePathStyle: true,
 });
