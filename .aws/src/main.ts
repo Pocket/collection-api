8e74d5bdccfb383d8e1b2e1d39301b8e0f21f7b7
Kelvin Yeboah <kyeboah@getpocket.com>
2021-05-10 10:12:56 -0400

    feat(ci/cd): update CI/CD to use codepipeline

--- .aws/src/main.ts
+++ .aws/src/main.ts
@@ -1,11 +1,27 @@
 import { Construct } from 'constructs';
-import { App, RemoteBackend, TerraformStack } from 'cdktf';
-import { AwsProvider, S3Bucket } from '../.gen/providers/aws';
+import {
+  App,
+  DataTerraformRemoteState,
+  RemoteBackend,
+  TerraformStack,
+} from 'cdktf';
+import {
+  AwsProvider,
+  DataAwsCallerIdentity,
+  DataAwsKmsAlias,
+  DataAwsRegion,
+  DataAwsSnsTopic,
+  S3Bucket,
+} from '../.gen/providers/aws';
 import { config } from './config';
-import { PocketVPC } from '@pocket/terraform-modules';
+import {
+  ApplicationRDSCluster,
+  PocketALBApplication,
+  PocketECSCodePipeline,
+  PocketPagerDuty,
+  PocketVPC,
+} from '@pocket/terraform-modules';
 import { PagerdutyProvider } from '../.gen/providers/pagerduty';
-import { createRds } from './rds';
-import { createPocketAlbApplication } from './pocketAlbApplication';
 
 class CollectionAPI extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -22,14 +38,288 @@ class CollectionAPI extends TerraformStack {
     });
 
     const pocketVpc = new PocketVPC(this, 'pocket-vpc');
+    const region = new DataAwsRegion(this, 'region');
+    const caller = new DataAwsCallerIdentity(this, 'caller');
 
-    const rds = createRds(this, pocketVpc);
-
-    const s3 = new S3Bucket(this, 'image-uploads', {
-      bucket: `pocket-${config.prefix.toLowerCase()}-images`,
+    const pocketApp = this.createPocketAlbApplication({
+      rds: this.createRds(pocketVpc),
+      s3: this.createS3Bucket(),
+      pagerDuty: this.createPagerDuty(),
+      secretsManagerKmsAlias: this.getSecretsManagerKmsAlias(),
+      snsTopic: this.getCodeDeploySnsTopic(),
+      region,
+      caller,
     });
 
-    createPocketAlbApplication(this, { rds, s3 });
+    this.createApplicationCodePipeline(pocketApp);
+  }
+
+  /**
+   * Get the sns topic for code deploy
+   * @private
+   */
+  private getCodeDeploySnsTopic() {
+    return new DataAwsSnsTopic(this, 'backend_notifications', {
+      name: `Backend-${config.environment}-ChatBot`,
+    });
+  }
+
+  /**
+   * Get secrets manager kms alias
+   * @private
+   */
+  private getSecretsManagerKmsAlias() {
+    return new DataAwsKmsAlias(this, 'kms_alias', {
+      name: 'alias/aws/secretsmanager',
+    });
+  }
+
+  /**
+   * Create S3 bucket for image uploads
+   * @private
+   */
+  private createS3Bucket() {
+    return new S3Bucket(this, 'image-uploads', {
+      bucket: `pocket-${config.prefix.toLowerCase()}-images`,
+    });
+  }
+
+  /**
+   * Creat Aurora database
+   * @param pocketVpc
+   * @private
+   */
+  private createRds(pocketVpc: PocketVPC) {
+    return new ApplicationRDSCluster(this, 'rds', {
+      prefix: config.prefix,
+      vpcId: pocketVpc.vpc.id,
+      subnetIds: pocketVpc.privateSubnetIds,
+      rdsConfig: {
+        databaseName: 'collections',
+        masterUsername: 'pkt_collections',
+        engine: 'aurora-mysql',
+        engineMode: 'serverless',
+        scalingConfiguration: [
+          {
+            minCapacity: config.rds.minCapacity,
+            maxCapacity: config.rds.maxCapacity,
+            autoPause: false,
+          },
+        ],
+      },
+
+      tags: config.tags,
+    });
+  }
+
+  /**
+   * Create CodePipeline to build and deploy terraform and ecs
+   * @param app
+   * @private
+   */
+  private createApplicationCodePipeline(app: PocketALBApplication) {
+    new PocketECSCodePipeline(this, 'code-pipeline', {
+      prefix: config.prefix,
+      source: {
+        codeStarConnectionArn: config.codePipeline.githubConnectionArn,
+        repository: config.codePipeline.repository,
+        branchName: config.codePipeline.branch,
+      },
+    });
+  }
+
+  /**
+   * Create PagerDuty service for alerts
+   * @private
+   */
+  private createPagerDuty() {
+    const incidentManagement = new DataTerraformRemoteState(
+      this,
+      'incident_management',
+      {
+        organization: 'Pocket',
+        workspaces: {
+          name: 'incident-management',
+        },
+      }
+    );
+
+    return new PocketPagerDuty(this, 'pagerduty', {
+      prefix: config.prefix,
+      service: {
+        criticalEscalationPolicyId: incidentManagement.get(
+          'policy_backend_critical_id'
+        ),
+        nonCriticalEscalationPolicyId: incidentManagement.get(
+          'policy_backend_non_critical_id'
+        ),
+      },
+    });
+  }
+
+  private createPocketAlbApplication(dependencies: {
+    rds: ApplicationRDSCluster;
+    s3: S3Bucket;
+    pagerDuty: PocketPagerDuty;
+    region: DataAwsRegion;
+    caller: DataAwsCallerIdentity;
+    secretsManagerKmsAlias: DataAwsKmsAlias;
+    snsTopic: DataAwsSnsTopic;
+  }): PocketALBApplication {
+    const {
+      rds,
+      s3,
+      pagerDuty,
+      region,
+      caller,
+      secretsManagerKmsAlias,
+      snsTopic,
+    } = dependencies;
+
+    return new PocketALBApplication(this, 'application', {
+      internal: true,
+      prefix: config.prefix,
+      alb6CharacterPrefix: config.shortName,
+      tags: config.tags,
+      cdn: false,
+      domain: config.domain,
+      containerConfigs: [
+        {
+          name: 'app',
+          portMappings: [
+            {
+              hostPort: 4004,
+              containerPort: 4004,
+            },
+          ],
+          healthCheck: {
+            command: [
+              'CMD-SHELL',
+              'curl -f http://localhost:4004/.well-known/apollo/server-health || exit 1',
+            ],
+            interval: 15,
+            retries: 3,
+            timeout: 5,
+            startPeriod: 0,
+          },
+          envVars: [
+            {
+              name: 'NODE_ENV',
+              value: process.env.NODE_ENV, // this gives us a nice lowercase production and development
+            },
+            {
+              name: 'AWS_S3_BUCKET',
+              value: s3.id,
+            },
+          ],
+          secretEnvVars: [
+            {
+              name: 'SENTRY_DSN',
+              valueFrom: `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/SENTRY_DSN`,
+            },
+            {
+              name: 'DATABASE_URL',
+              valueFrom: `${rds.secretARN}:database_url::`,
+            },
+          ],
+        },
+        {
+          name: 'xray-daemon',
+          containerImage: 'amazon/aws-xray-daemon',
+          repositoryCredentialsParam: `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared/DockerHub`,
+          portMappings: [
+            {
+              hostPort: 2000,
+              containerPort: 2000,
+              protocol: 'udp',
+            },
+          ],
+          command: ['--region', 'us-east-1', '--local-mode'],
+        },
+      ],
+      codeDeploy: {
+        useCodeDeploy: true,
+        useCodePipeline: true,
+        snsNotificationTopicArn: snsTopic.arn,
+      },
+      exposedContainer: {
+        name: 'app',
+        port: 4004,
+        healthCheckPath: '/.well-known/apollo/server-health',
+      },
+      ecsIamConfig: {
+        prefix: config.prefix,
+        taskExecutionRolePolicyStatements: [
+          //This policy could probably go in the shared module in the future.
+          {
+            actions: ['secretsmanager:GetSecretValue', 'kms:Decrypt'],
+            resources: [
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared`,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared/*`,
+              secretsManagerKmsAlias.targetKeyArn,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:${config.name}/${config.environment}`,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:${config.name}/${config.environment}/*`,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:${config.prefix}`,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:${config.prefix}/*`,
+            ],
+            effect: 'Allow',
+          },
+          //This policy could probably go in the shared module in the future.
+          {
+            actions: ['ssm:GetParameter*'],
+            resources: [
+              `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}`,
+              `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/*`,
+            ],
+            effect: 'Allow',
+          },
+        ],
+        taskRolePolicyStatements: [
+          {
+            actions: [
+              'xray:PutTraceSegments',
+              'xray:PutTelemetryRecords',
+              'xray:GetSamplingRules',
+              'xray:GetSamplingTargets',
+              'xray:GetSamplingStatisticSummaries',
+            ],
+            resources: ['*'],
+            effect: 'Allow',
+          },
+          {
+            actions: ['s3:*'],
+            resources: [`arn:aws:s3:::${s3.id}`, `arn:aws:s3:::${s3.id}/*`],
+            effect: 'Allow',
+          },
+        ],
+        taskExecutionDefaultAttachmentArn:
+          'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy',
+      },
+
+      autoscalingConfig: {
+        targetMinCapacity: 2,
+        targetMaxCapacity: 10,
+      },
+      alarms: {
+        //TODO: When we start using this more we will change from non-critical to critical
+        http5xxError: {
+          threshold: 10,
+          evaluationPeriods: 2,
+          period: 600,
+          actions: [pagerDuty.snsNonCriticalAlarmTopic.arn],
+        },
+        httpLatency: {
+          evaluationPeriods: 2,
+          threshold: 500,
+          actions: [pagerDuty.snsNonCriticalAlarmTopic.arn],
+        },
+        httpRequestCount: {
+          threshold: 5000,
+          evaluationPeriods: 2,
+          actions: [pagerDuty.snsNonCriticalAlarmTopic.arn],
+        },
+      },
+    });
   }
 }
 
