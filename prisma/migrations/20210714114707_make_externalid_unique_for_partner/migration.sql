cefe735cb095fedb1fabaeeb811228afb9cc641f
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2021-07-16 22:06:22 +1000

    Consolidated migrations

--- prisma/migrations/20210714114707_make_externalid_unique_for_partner/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[externalId]` on the table `CollectionPartner` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `externalId` ON `CollectionPartner`(`externalId`);
