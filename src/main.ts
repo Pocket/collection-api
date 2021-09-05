583d02c2f448ffcb5f76d72a31810959ea50974f
Jonathan Petto <jpetto@users.noreply.github.com>
2021-09-05 14:42:18 -0500

    disable xray

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 import * as Sentry from '@sentry/node';
 import config from './config';
-import AWSXRay from 'aws-xray-sdk-core';
-import xrayExpress from 'aws-xray-sdk-express';
+//import AWSXRay from 'aws-xray-sdk-core';
+//import xrayExpress from 'aws-xray-sdk-express';
 import express from 'express';
-import https from 'https';
+//import https from 'https';
 import { graphqlUploadExpress } from 'graphql-upload';
 import { server as adminServer } from './admin/server';
 import { server as publicServer } from './public/server';
 
 //Set XRAY to just log if the context is missing instead of a runtime error
-AWSXRay.setContextMissingStrategy('LOG_ERROR');
+//AWSXRay.setContextMissingStrategy('LOG_ERROR');
 
 //Add the AWS XRAY ECS plugin that will add ecs specific data to the trace
-AWSXRay.config([AWSXRay.plugins.ECSPlugin]);
+//AWSXRay.config([AWSXRay.plugins.ECSPlugin]);
 
 //Capture all https traffic this service sends
 //This is to auto capture node fetch requests (like to parser)
-AWSXRay.captureHTTPsGlobal(https, true);
+//AWSXRay.captureHTTPsGlobal(https, true);
 
 //Capture all promises that we make
-AWSXRay.capturePromise();
+//AWSXRay.capturePromise();
 
 // Initialize sentry
 Sentry.init({
@@ -30,10 +30,10 @@ Sentry.init({
 const app = express();
 
 //If there is no host header (really there always should be..) then use collection-api as the name
-app.use(xrayExpress.openSegment('collections-api'));
+//app.use(xrayExpress.openSegment('collections-api'));
 
 //Set XRay to use the host header to open its segment name.
-AWSXRay.middleware.enableDynamicNaming('*');
+//AWSXRay.middleware.enableDynamicNaming('*');
 
 // Upload middleware
 app.use(
@@ -50,7 +50,7 @@ adminServer.applyMiddleware({ app, path: '/admin' });
 publicServer.applyMiddleware({ app, path: '/' });
 
 //Make sure the express app has the xray close segment handler
-app.use(xrayExpress.closeSegment());
+//app.use(xrayExpress.closeSegment());
 
 // The `listen` method launches a web server.
 app.listen({ port: 4004 }, () => {
