66825bbc1def81fd61f9f84549f6aa3cba1bee34
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2021-09-13 20:05:33 +1000

    chore(tests) Add server integration tests for CollectionPartner
    - Added tests for queries and mutations that are used by Curation Tools Frontend.

--- src/test/admin-server/queries/queries.gql.ts
+++ src/test/admin-server/queries/queries.gql.ts
@@ -37,3 +37,35 @@ export const GET_COLLECTION_AUTHOR = gql`
     }
   }
 `;
+
+export const GET_COLLECTION_PARTNERS = gql`
+  query getCollectionPartners($page: Int, $perPage: Int) {
+    getCollectionPartners(page: $page, perPage: $perPage) {
+      partners {
+        externalId
+        name
+        url
+        imageUrl
+        blurb
+      }
+      pagination {
+        currentPage
+        totalPages
+        totalResults
+        perPage
+      }
+    }
+  }
+`;
+
+export const GET_COLLECTION_PARTNER = gql`
+  query getCollectionPartner($id: String!) {
+    getCollectionPartner(externalId: $id) {
+      externalId
+      name
+      url
+      imageUrl
+      blurb
+    }
+  }
+`;
